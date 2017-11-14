17f7baa53c47104e107701095b6847a9cb755827
Hilary Claire <hilary.wilson2563@gmail.com>
2017-11-13 21:02:05 -0500

    add animation

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ $(document).ready(function() {
 var width;
 var height;
 var div;
+
 $(".container div").on("click", function() {
   if($(this).attr("value") !== "1"){
   $(".container div").attr("value", "0");
   width = $(this).width();
   height = $(this).height();
   div = this;
-  $(this).css("width", "100%");
-  $(this).css("height","100%");
+  $(this).animate({width:"100%",height:"100%"}, 250);
+  setTimeout(function(){$("p").css("display","inline-block");}, 200);
   $(this).css("border-radius", "10px");
   $(this).css("z-index", "10");
   $(this).attr("value", "1");
-  $("p").css("display","inline-block");
+  
   return;
   }
 })
 $("p").on("click", function () {
-  $(div).css("width", width);
-  $(div).css("height", height);
+  $(div).animate({width: width, height: height},500);
   $("p").css("display", "none");
   $(div).css("border-top-right-radius", "0px");
   $(div).css("border-bottom-left-radius", "0px");
