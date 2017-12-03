6c9cbc4cc6ae1a1d8131933d8812d1530ab7c7c6
Hilary Claire <hilary.wilson2563@gmail.com>
2017-12-02 19:21:12 -0500

    functionality more user friendly with double click

--- script.js
+++ script.js
@@ -3,20 +3,25 @@ var width;
 var height;
 var div;
 
+
 $(".container div").on("click", function() {
-  if($(this).attr("value") !== "1"){
-  $(".container div").attr("value", "0");
-  width = $(this).width();
-  height = $(this).height();
-  div = this;
-  $(this).animate({width:"100%",height:"100%"}, 250);
-  setTimeout(function(){$("p").css("display","inline-block");}, 200);
-  $(this).css("border-radius", "10px");
-  $(this).css("z-index", "10");
-  $(this).attr("value", "1");
-  
-  return;
-  }
+   if($(this).attr("value") === "1" && $("p").css("display") === "none"){
+    $(".container div").attr("value", "0");
+    return;
+    }
+    if($(this).attr("value") !== "1"){
+      $(".container div").attr("value", "0");
+      width = $(this).width();
+      height = $(this).height();
+      div = this;
+      console.log({width, height, div});
+      $(this).animate({width:"100%",height:"100%"}, 250);
+      setTimeout(function(){$("p").css("display","inline-block");}, 200);
+      $(this).css("border-radius", "10px");
+      $(this).css("z-index", "10");
+      $(this).attr("value", "1");    
+      return;
+    }
 })
 $("p").on("click", function () {
   $(div).animate({width: width, height: height},500);
@@ -24,7 +29,17 @@ $("p").on("click", function () {
   $(div).css("border-top-right-radius", "0px");
   $(div).css("border-bottom-left-radius", "0px");
   $(div).css("z-index", "2");
+
 })
+$(".container div" ).dblclick(function() {
+  $(div).animate({width: width, height: height},500);
+  console.log("hello");
+  $("p").css("display", "none");
+  $(div).css("border-top-right-radius", "0px");
+  $(div).css("border-bottom-left-radius", "0px");
+  $(div).css("z-index", "2");
+    return;
+    });
 
 
 });
